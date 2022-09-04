1d1b8c3a8aa293491c248fe223c3e48c50d3947b
Andrea <andrea@interhact.net>
2022-09-03 22:57:02 -0500

    work on server file

---
+++ server.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const path = require("path");
+const fs = require('fs');
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    res.status(200).json(`${req.method} request received to get notes`);
+    console.info(`${req.method} request received to get notes`);
+});
+
+// POST request to add a note
+app.post('/api/notes', (req, res) => {
+    const { title, text } = req.body;
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+        };
+
+        // Obtain existing notes
+        fs.readFile('db/db.json', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                // Convert string into JSON object
+                const parsedNotes = JSON.parse(data);
+
+                // Add a new note
+                parsedNotes.push(newNote);
+
+                // Write updated notes back to the file
+                fs.writeFile(
+                    'db/db.json',
+                    JSON.stringify(parsedNotes, null, 4),
+                    (writeErr) =>
+                        writeErr
+                            ? console.error(writeErr)
+                            : console.info('Successfully updated reviews!')
+                );
+            }
+        });
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        console.log(response);
+        res.status(201).json(response);
+    } else {
+        res.status(500).json('Error in posting note');
+    }
+});
+
+
+
+
+
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT}`)
+);
