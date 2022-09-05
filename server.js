4c0a7a7568be680df3b706eef363ad5cbe9470fe
Andrea <andrea@interhact.net>
2022-09-04 21:47:58 -0500

    reworking server file

--- server.js
+++ server.js
@@ -19,51 +19,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-// not exactly sure what to do here
-app.get('/api/notes', (req, res) => {
-    res.status(200).json(`${req.method} request received to get notes`);
-    console.info(`${req.method} request received to get notes`);
-});
-
-
-app.post('/api/notes', (req, res) => {
-    const { title, text } = req.body;
-    if (title && text) {
-        const newNote = {
-            title,
-            text,
-        };
-
-
-        fs.readFile('db/db.json', 'utf8', (err, data) => {
-            if (err) {
-                console.error(err);
-            } else {
-                const parsedNotes = JSON.parse(data);
-                parsedNotes.push(newNote);
-                fs.writeFile(
-                    'db/db.json',
-                    JSON.stringify(parsedNotes, null, 4),
-                    (writeErr) =>
-                        writeErr
-                            ? console.error(writeErr)
-                            : console.info('Successfully updated reviews!')
-                );
-            }
-        });
-
-        const response = {
-            status: 'success',
-            body: newNote,
-        };
-
-        console.log(response);
-        res.status(201).json(response);
-    } else {
-        res.status(500).json('Error in posting note');
-    }
-});
-
 
 
 
